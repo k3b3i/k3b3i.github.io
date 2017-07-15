bb804d049f0d3270ac9d1eee291011e7105e8d2d
tzarras <TimKitchen@Tims-MacBook-Pro.local>
2017-07-15 12:56:05 -0400

    yo

--- CCH.js
+++ CCH.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
         gallery.append(images);
 
         
-        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+        if($(window).width() < 767) {
           
           $(info).appendTo(body).insertAfter(".phonesplash");
 
@@ -52,79 +52,336 @@ $(document).ready(function(){
 
 
 
-    $(".down,.arrow").click(function(){
+    $(".down").click(function(){
       var one = $(".desc").offset();
-      var two = $(window).height()*.33;
+      var two = $(".desc").height()*.5;
       $(".arrow").fadeOut();
         $("body,html").animate({scrollTop: one.top-two},700);
     });
 
-    $(".purchase").click(function(){
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+
+      $(".purchase").click(function(){
+      var one = $(".contain").offset();
+        $("body,html").animate({scrollTop: one.top+100},700);
+    });
+
+    } else {
+
+      $(".purchase").click(function(){
       var one = $(".contain").offset();
       var two = $(window).height()*.15;
         $("body,html").animate({scrollTop: one.top-two},700);
     });
 
+    }
+
     $(".gallery").click(function(){
       var one = $(".imagegallery").offset();
-      var two = $(window).height()*.05;
-        $("body,html").animate({scrollTop: one.top-two},700);
+        $("body,html").animate({scrollTop: one.top-100},700);
     });
 
     $(".cart").click(function(){
       $(".cartpage").toggleClass("out");
     });
 
-    var price = 39;
 
-    $(".buyone").click(function(){
+
+
+
+
+
+
+
+    var price = 39;
+    var copperP = parseInt($('.thispricec').text());
+    var brassP = parseInt($('.thispriceb').text());
+    var stainlessP = parseInt($('.thisprices').text());
+    var totalP = copperP+brassP+stainlessP;
+
+
+    $(".buyone",this).click(function(){
+
+        $(".thispricec").html(function(i, val) { 
+          return val*1+price
+        });
+        
+        var copperP = parseInt($('.thispricec').text());
+        var brassP = parseInt($('.thispriceb').text());
+        var stainlessP = parseInt($('.thisprices').text());
+        var totalP = copperP+brassP+stainlessP;
+
+        $(".totalnumber > p").html(function() { 
+          return totalP 
+        });
+        
+        $(".empty").hide();
+
+        $(".qtyc").html(function(i, val) { 
+          return val*1+1
+        });
+
+        $(".shipping,.cartcontent,.cartitemcopper,.checkout").show();
 
         $(".cartcount").html(function(i, val) { 
           return val*1+1 
-        });
-
-        $(".totalnumber > p").html(function(i, val) {
-          return val*1+price
-        });
-
-        $( ".cartpage" ).append("<div class='cartitem'><p class='producttitle'>Copper</p><p class='price'>$39</p><p class='removeone'>Remove</p></div>");
-          //add variant to array//
-          cartItems.push('35068487235:1');
-
+         });
+   
     });
 
     
     $(".buytwo").click(function(){
+        
+        $(".thispriceb").html(function(i, val) { 
+          return val*1+price
+        });
+
+        var copperP = parseInt($('.thispricec').text());
+        var brassP = parseInt($('.thispriceb').text());
+        var stainlessP = parseInt($('.thisprices').text());
+        var totalP = copperP+brassP+stainlessP;
+
+        $(".totalnumber > p").html(function() { 
+          return totalP
+        });
+
+        $(".empty").hide();
+    
+        $(".qtyb").html(function(i, val) { 
+          return val*1+1
+        });
+
+        $(".shipping,.cartcontent,.cartitembrass,.checkout").show();
 
         $(".cartcount").html(function(i, val) { 
-          return val*1+1 });
-
-        $(".totalnumber > p").html(function(i, val) {
-          return val*1+price
-          });
-
-        $( ".cartpage" ).append("<div class='cartitem'><p class='producttitle'>Brass</p><p class='price'>$39</p><p class='removetwo'>Remove</p></div>");
-          //add variant to array//
-          cartItems.push('35068360067:1');
+          return val*1+1 
+        });
 
     });
 
     
     $(".buythree").click(function(){
+        
+        $(".thisprices").html(function(i, val) { 
+          return val*1+price
+        });
+
+        var copperP = parseInt($('.thispricec').text());
+        var brassP = parseInt($('.thispriceb').text());
+        var stainlessP = parseInt($('.thisprices').text());
+        var totalP = copperP+brassP+stainlessP;
+
+        $(".totalnumber > p").html(function() { 
+          return totalP
+        });
+
+        $(".empty").hide();
+
+        $(".qtys").html(function(i, val) { 
+          return val*1+1
+        });
+
+        $(".shipping,.cartcontent,.cartitemstainless,.checkout").show();
 
         $(".cartcount").html(function(i, val) {
           return val*1+1 
           });
 
-        $(".totalnumber > p").html(function(i, val) {
-          return val*1+price
-          });
+    });
 
-        $( ".cartpage" ).append("<div class='cartitem'><p class='producttitle'>Stainless</p><p class='price'>$39</p><p class='removethree'>Remove</p></div>");
-          //add variant to array//
-          cartItems.push('35068492547:1');
+    // update the cart total whenever anything is clicked within cart //
+
+    $(".cartcontent").click(function(){
+
+        var copperP = parseInt($('.thispricec').text());
+        var brassP = parseInt($('.thispriceb').text());
+        var stainlessP = parseInt($('.thisprices').text());
+        var totalP = copperP+brassP+stainlessP;
+
+        $(".totalnumber > p").html(function() {
+                  return totalP;
+                  });
+
+        
+    });
+
+    $(".cartpage").click(function(){
+      if ($(".cartitembrass").css('display')=== 'none' && $(".cartitemstainless").css('display')=== 'none' && $(".cartitemcopper").css('display')=== 'none') {
+          $(".cartcontent,.shipping,.checkout").hide();
+          $(".empty").show();
+          $(".cartcount").text("0");
+          console.log("wtf");
+        }
+      });
+
+    // quantity //
+
+    var total;
+
+    //copper//
+
+    $('.qtyplusc').click(function(){
+              
+              $(".qtyc").html(function(i, val) {
+                  return val*1+1
+                  });
+              var one = parseInt($('.qtyc').text());
+              var two = price;
+              var total = one*two;
+              console.log(one);
+              console.log(two);
+              console.log(total);
+              
+              $(".pricec > p").html(function(i, val) {
+                  return total
+                  });
 
     });
+
+    $(".qtyminusc").click(function(e) {
+      
+      $(".qtyc").html(function(i, val) {
+          return val*1-1
+      });
+      
+      var one = parseInt($('.qtyc').text());
+      var two = price;
+      var total = one*two;
+      console.log(one);
+      console.log(two);
+      console.log(total);
+      
+      $(".pricec > p").html(function(i, val) {
+          return total
+          });
+
+      if (one===0){
+        $(".cartitemcopper").hide();
+      } else {
+        $(".cartitemcopper").show();
+      }
+      
+      });
+
+    //brass//
+
+      $('.qtyplusb').click(function(){
+      
+      $(".qtyb").html(function(i, val) {
+          return val*1+1
+          });
+
+      $(".totalnumber > p").html(function(i, val) {
+          return total;
+          });
+      
+      
+      var one = parseInt($('.qtyb').text());
+      var two = price;
+      var total = one*two;
+      console.log(one);
+      console.log(two);
+      console.log(total);
+      
+      $(".priceb > p").html(function(i, val) {
+          return total
+          });
+
+        
+    });
+
+    $(".qtyminusb").click(function(e) {
+      
+      $(".qtyb").html(function(i, val) {
+          return val*1-1
+      });
+      
+      var one = parseInt($('.qtyb').text());
+      var two = price;
+      var total = one*two;
+      console.log(one);
+      console.log(two);
+      console.log(total);
+      
+      $(".priceb > p").html(function(i, val) {
+          return total
+          });
+
+      if (one===0){
+        $(".cartitembrass").hide();
+      } else {
+        $(".cartitembrass").show();
+      }
+      
+      });
+
+    //stainless//
+
+    $('.qtypluss').click(function(){
+      
+      $(".qtys").html(function(i, val) {
+          return val*1+1
+          });
+
+      $(".totalnumber > p").html(function(i, val) {
+          return total;
+          });
+      
+      
+      var one = parseInt($('.qtys').text());
+      var two = price;
+      var total = one*two;
+      console.log(one);
+      console.log(two);
+      console.log(total);
+      
+      $(".prices > p").html(function(i, val) {
+          return total
+          });
+
+        
+    });
+
+    $(".qtyminuss").click(function(e) {
+      
+      $(".qtys").html(function(i, val) {
+          return val*1-1
+      });
+      
+      var one = parseInt($('.qtys').text());
+      var two = price;
+      var total = one*two;
+      console.log(one);
+      console.log(two);
+      console.log(total);
+      
+      $(".prices > p").html(function(i, val) {
+          return total
+          });
+
+      if (one===0){
+        $(".cartitemstainless").hide();
+      } else {
+        $(".cartitemstainless").show();
+      }
+      
+      });
+
+
+    // quantity buttons and cart count //
+
+    $(".qtyplusc,.qtyplusb,.qtypluss").click(function(){
+      $(".cartcount").html(function(i, val) {
+          return val*1+1 
+        });
+    });
+
+    $(".qtyminusc,.qtyminusb,.qtyminuss").click(function(){
+      $(".cartcount").html(function(i, val) {
+          return val*1-1 
+        });
+    });
+
 
     //close cart page//
 
@@ -135,52 +392,40 @@ $(document).ready(function(){
     //remove copper items from cart//
     
     $('.cartpage').on('click','.removeone',function(){
-    
-        $(this).parents('.cartitem').remove();
+      var copperQ = parseInt($('.qtyc').text());
+      var copperP= parseInt($('.thispricec').text());
+        $('.cartitemcopper').hide();
         //minus 1 from cart count//
         $(".cartcount").html(function(i, val) {
-          return val*1-1 
+          return val*1-copperQ 
         });
         //subtract 40 from total//
         $(".totalnumber > p").html(function(i, val) {
-          return val*1-price 
+          return val*1-copperP 
         });
 
-        var search_term = '35068487235:1';
-
-
-        for (var i=cartItems.length-1; i>=0; i--) {
-            if (cartItems[i] === search_term) {
-                cartItems.splice(i, 1);
-                break;    
-            }
-        }
+        $(".qtyc").text("0");
+        $(".thispricec").text("0");
 
     });
 
     //remove brass items from cart//
 
     $('.cartpage').on('click','.removetwo',function(){
-    
-        $(this).parents('.cartitem').remove();
+        var brassQ = parseInt($('.qtyb').text());
+        var brassP= parseInt($('.thispriceb').text());
+        $('.cartitembrass').hide();
         //minus 1 from cart count//
         $(".cartcount").html(function(i, val) {
-          return val*1-1 
+          return val*1-brassQ 
         });
         //subtract 40 from total//
         $(".totalnumber > p").html(function(i, val) {
-          return val*1-price 
+          return val*1-brassP 
         });
 
-        var search_term = '35068360067:1';
-
-
-        for (var i=cartItems.length-1; i>=0; i--) {
-            if (cartItems[i] === search_term) {
-                cartItems.splice(i, 1);
-                break;    
-            }
-        }
+        $(".qtyb").text("0");
+        $(".thispriceb").text("0");
 
 
     });
@@ -188,39 +433,33 @@ $(document).ready(function(){
     //remove stainless items from cart//
 
     $('.cartpage').on('click','.removethree',function(){
-    
-        $(this).parents('.cartitem').remove();
+        var stainlessQ = parseInt($('.qtys').text());
+        var stainlessP= parseInt($('.thisprices').text());
+        $('.cartitemstainless').hide();
         //minus 1 from cart count//
         $(".cartcount").html(function(i, val) {
-          return val*1-1 
+          return val*1-stainlessQ 
         });
         //subtract 40 from total//
         $(".totalnumber > p").html(function(i, val) {
-          return val*1-price 
+          return val*1-stainlessP 
         });
 
-        var search_term = '35068492547:1';
+        $(".qtys").text("0");
+        $(".thisprices").text("0");
 
-        for (var i=cartItems.length-1; i>=0; i--) {
-            if (cartItems[i] === search_term) {
-                cartItems.splice(i, 1);
-                break;    
-            }
-        }
 
     });
 
-    $(".buyone,.buytwo,.buythree,.removeone,.removetwo,.removethree").click(function(){
-
-       if( $('.cartitem').length = 0 ){
-            $(".checkout").hide();
-            $(".empty").show();
+    // refresh on smaller size, just in case //
+    $(window).resize(function(){
+          if ($(window).width() <= 767) {   
+                  location.reload(); 
         } else {
-            $(".checkout").show();
-            $(".empty").hide();
-        }
 
+        }
     });
+  
 
     var cartItems = [];
     var shopifyLink = "http://nihil-artikel.myshopify.com/cart/";
@@ -228,12 +467,46 @@ $(document).ready(function(){
     // generate final permalink //
 
     $(".checkout").click(function() {
+        var skuC = "35068487235:";
+        var skuB = "35068360067:";
+        var skuS = "35068492547:";
+        var e = ",";
+        var f = ",";
+        var g = ",";
+        var copperQ = parseInt($('.qtyc').text());
+        var brassQ = parseInt($('.qtyb').text());
+        var stainlessQ = parseInt($('.qtys').text());
         var link = shopifyLink;
-        cartItems.forEach(function(item) {
-            link += item + ',';
-        });
+        var finalLink = shopifyLink + skuC + copperQ + e + skuB + brassQ + f + skuS + stainlessQ;
+
+        if (copperQ===0) {
+          console.log("no copper!");
+          finalLink = finalLink.replace('35068487235:0,','');
+        }
+
+        if (brassQ===0) {
+          console.log("no brass!");
+          finalLink = finalLink.replace(',35068360067:0','').replace('35068360067:0,','');
+        } 
+
+        if (stainlessQ===0) {
+          console.log("no stainless!");
+          finalLink = finalLink.replace(',35068492547:0','');
+         } 
+         //else if (stainlessQ >= 1) {
+        //   console.log("wtf");
+        //   finalLink = finalLink.replace(',','');
+        // }
+
+
+        console.log(copperQ);
+        console.log(brassQ);
+        console.log(stainlessQ);
+        console.log(finalLink);
+        console.log(cartItems);
+
         //direct browser to generated link//
-        window.location.href = link;
+        window.location.href = finalLink;
     });
 
 
