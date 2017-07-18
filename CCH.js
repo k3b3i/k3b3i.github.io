74ddf8ea418d5784f142df34bf7231287bf3dcd0
tzarras <TimKitchen@Tims-MacBook-Pro.local>
2017-07-18 12:01:00 -0400

    scroll fix

--- CCH.js
+++ CCH.js
@@ -93,7 +93,7 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
 
     $(".down").click(function(){
       var one = $(".desc").offset();
-      var two = $(".contain").height()*.35;
+      var two = $(".contain").height()*.375;
       $(".arrow").fadeOut();
         $("body,html").animate({scrollTop: one.top-two},700);
     });
